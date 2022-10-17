e87189347bae77fa1e7d9d1a74e9e963d6a88169
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-17 20:50:32 +0100

    Installed react-markdown library

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import ReactMarkdown from "react-markdown";
 
 function App() {
   const [markdown, setMarkdown] = useState("## Markdown preview");
-  return <h2>markdown preview starter</h2>;
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        >
+        </textarea>
+        <article className="result">
+          {markdown}
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;

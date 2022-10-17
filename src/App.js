3bed3db8dfcf57093fee195896e5fdcf76a1d514
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-17 20:43:50 +0100

    set up markdown state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [markdown, setMarkdown] = useState("## Markdown preview");
   return <h2>markdown preview starter</h2>;
 }
 
 export default App;
-
